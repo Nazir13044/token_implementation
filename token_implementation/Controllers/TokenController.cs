8faeccce1bcb53661ce6dabad2abc8fbc997b9a6
Nazir13044 <rokoncse13044@gmail.com>
2024-09-17 22:51:57 +0600

    Add project files.

---
+++ token_implementation/Controllers/TokenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace token_implementation.Controllers;
+[Route("api/[controller]")]
+public class TokenController : Controller
+{
+    [HttpGet("")]
+    public string Get()
+    {
+        return "Ok();";
+    }
+}
