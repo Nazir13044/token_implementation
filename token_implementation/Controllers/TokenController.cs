969ae6e70a542644b4a8b0c320c234d33f6f314c
Nazir13044 <rokoncse13044@gmail.com>
2024-09-18 22:07:51 +0600

    implement jwt token using identity model

--- token_implementation/Controllers/TokenController.cs
+++ token_implementation/Controllers/TokenController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace token_implementation.Controllers;
+
 [Route("api/[controller]")]
+[ApiController]
+[Authorize]
 public class TokenController : Controller
 {
-    [HttpGet("")]
-    public string Get()
+    [HttpGet]
+    public IActionResult Get()
     {
-        return "Ok();";
+        return Ok("Hello World");
     }
 }
